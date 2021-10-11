7ae1205309121605b4f5964f8b52fbe4c2bb7f17
Unbound <unbound9dev@gmail.com>
2021-10-11 16:39:41 +0300

    finished ex3

--- src/components/ShoeIndex/ShoeIndex.js
+++ src/components/ShoeIndex/ShoeIndex.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from 'react'
+import styled from 'styled-components/macro'
 
-import { WEIGHTS } from '../../constants';
+import { WEIGHTS, QUERIES } from '../../constants'
 
-import Breadcrumbs from '../Breadcrumbs';
-import Select from '../Select';
-import Spacer from '../Spacer';
-import ShoeSidebar from '../ShoeSidebar';
-import ShoeGrid from '../ShoeGrid';
+import Breadcrumbs from '../Breadcrumbs'
+import Select from '../Select'
+import Spacer from '../Spacer'
+import ShoeSidebar from '../ShoeSidebar'
+import ShoeGrid from '../ShoeGrid'
 
 const ShoeIndex = ({ sortId, setSortId }) => {
   return (
@@ -16,12 +16,12 @@ const ShoeIndex = ({ sortId, setSortId }) => {
         <Header>
           <Title>Running</Title>
           <Select
-            label="Sort"
+            label='Sort'
             value={sortId}
             onChange={(ev) => setSortId(ev.target.value)}
           >
-            <option value="newest">Newest Releases</option>
-            <option value="price">Price</option>
+            <option value='newest'>Newest Releases</option>
+            <option value='price'>Price</option>
           </Select>
         </Header>
         <Spacer size={32} />
@@ -29,43 +29,55 @@ const ShoeIndex = ({ sortId, setSortId }) => {
       </MainColumn>
       <LeftColumn>
         <Breadcrumbs>
-          <Breadcrumbs.Crumb href="/">Home</Breadcrumbs.Crumb>
-          <Breadcrumbs.Crumb href="/sale">Sale</Breadcrumbs.Crumb>
-          <Breadcrumbs.Crumb href="/sale/shoes">
-            Shoes
-          </Breadcrumbs.Crumb>
+          <Breadcrumbs.Crumb href='/'>Home</Breadcrumbs.Crumb>
+          <Breadcrumbs.Crumb href='/sale'>Sale</Breadcrumbs.Crumb>
+          <Breadcrumbs.Crumb href='/sale/shoes'>Shoes</Breadcrumbs.Crumb>
         </Breadcrumbs>
-        <Spacer size={42} />
+        <VerticalSpacer size={42} />
         <ShoeSidebar />
       </LeftColumn>
     </Wrapper>
-  );
-};
+  )
+}
 
 const Wrapper = styled.div`
   display: flex;
   flex-direction: row-reverse;
   align-items: baseline;
   gap: 32px;
-`;
+
+  @media ${QUERIES.phone} {
+    flex-direction: column-reverse;
+    gap: 6px;
+  }
+`
 
 const LeftColumn = styled.div`
   flex-basis: 248px;
-`;
+
+  @media ${QUERIES.phone} {
+    flex-basis: 0px;
+  }
+`
 
 const MainColumn = styled.div`
   flex: 1;
-`;
+`
 
 const Header = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: baseline;
-`;
+`
 
 const Title = styled.h2`
   font-size: 1.5rem;
   font-weight: ${WEIGHTS.medium};
-`;
+`
 
-export default ShoeIndex;
+const VerticalSpacer = styled(Spacer)`
+  @media ${QUERIES.phone} {
+    display: none;
+  }
+`
+export default ShoeIndex
